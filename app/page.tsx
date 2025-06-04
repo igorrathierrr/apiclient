ddd4fe3c88073e8bbde282ed4df744c2ca7dec36
adairrohling <81537071+adairrohling@users.noreply.github.com>
2025-06-04 01:35:15 +0000

    feat: saveUniversity

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
    <>
    <h1>Página Inicial</h1> <br />
-   <a href="/university">Listar Universidades</a>
-
+   <a href="/university">Listar Universidades</a> <br />
+    <a href="/university/new">Cadastrar Universidade</a>
    </>
   );
 }
