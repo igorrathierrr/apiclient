8e053d32779e059ae5ccd01d2204fac7c0ec041d
adairrohling <81537071+adairrohling@users.noreply.github.com>
2025-06-14 01:29:21 +0000

    dix: ui2

--- app/university/delete/page.tsx
+++ app/university/delete/page.tsx
@@ -1,38 +1,12 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { useSearchParams,useRouter } from "next/navigation";
+import DeleteUniversity from "./DeleteUniversity";
 import { Suspense } from "react";
+    
+export default function Page() {
 
-
- function Page() {
-    const id = useSearchParams().get("id");
-    const router = useRouter();
-
-    async function handleDelete() {
-        console.log("Universidade deletada");
-        try {
-            const responde = await fetch(`https://serverapi-kappa.vercel.app/api/universities/${id}`, {
-                method: "DELETE",
-            })
-            router.push("/university");
-        } catch (error) {
-            console.error("Erro ao deletar a universidade:", error);
-        }
-    }
         return (
-            <div>
-                <h1>Deletar Universidade</h1>
-                <Button variant="destructive" onClick={handleDelete}>Deletar</Button>
-
-            </div>
-        )
-    }
-
-    export default function DeleteUniversity() {
-        return (
-            <Suspense >
+            <Suspense>
                 <DeleteUniversity />
             </Suspense>
+           
         );
-    }
+}
